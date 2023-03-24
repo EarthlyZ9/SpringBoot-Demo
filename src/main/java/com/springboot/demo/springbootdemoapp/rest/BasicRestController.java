7015adb4f7ef2265e297ce19d2a41eb037f110fa
linda2927 <linda2927@naver.com>
2023-03-24 23:25:44 +0900

    Spring Boot Basics: Add devtools, actuator, and security

--- src/main/java/com/springboot/demo/springbootdemoapp/rest/BasicRestController.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/rest/BasicRestController.java
@@ -1,13 +1,21 @@
 package com.springboot.demo.springbootdemoapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicRestController {
+    @Value("${myname}")
+    private String name;
 
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @GetMapping("/my-name")
+    public String getWorkout() {
+        return this.name;
     }
 }
