8e34b6fccb7c15a9864bcc3636682156a5eff0a2
linda2927 <linda2927@naver.com>
2023-03-25 00:52:57 +0900

    Spring Boot DI: check out bean lifecycle methods

--- src/main/java/com/springboot/demo/springbootdemoapp/coach/BaseballCoach.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/coach/BaseballCoach.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.springbootdemoapp.coach;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -7,11 +9,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Primary
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseballCoach implements Coach {
 
     public BaseballCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartUpStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println("In doMyCleanUpStuff(): " + getClass().getSimpleName());
     }
 
     @Override
