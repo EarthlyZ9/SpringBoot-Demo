5a8aedea45d68e09fe12b68c9127e6812cc2b02d
linda2927 <linda2927@naver.com>
2023-03-25 00:42:28 +0900

    Spring Boot DI: check out bean scope

--- src/main/java/com/springboot/demo/springbootdemoapp/coach/BaseballCoach.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/coach/BaseballCoach.java
@@ -1,10 +1,13 @@
 package com.springboot.demo.springbootdemoapp.coach;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseballCoach implements Coach {
 
     public BaseballCoach() {
