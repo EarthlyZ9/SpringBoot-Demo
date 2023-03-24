920d1fed2d3c3211180c86a6ecf502dc1c01c791
linda2927 <linda2927@naver.com>
2023-03-24 23:51:00 +0900

    Spring Boot DI: rename base package name

--- src/main/java/com/springboot/demo/springbootdemoapp/basic/BasicRestController.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/basic/BasicRestController.java
@@ -1,4 +1,4 @@
-package com.springboot.demo.springbootdemoapp.rest;
+package com.springboot.demo.springbootdemoapp.basic;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
