c245bdcdb0edfe935e08b813fca1d00e086ecdb9
linda2927 <linda2927@naver.com>
2023-03-24 23:49:54 +0900

    Spring Boot DI: Autowiring with constructors

---
+++ src/main/java/com/springboot/demo/springbootdemoapp/coach/CoachController.java
@@ -0,0 +1,21 @@
+package com.springboot.demo.springbootdemoapp.coach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoachController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public CoachController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/daily-workout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
---
+++ src/main/java/com/springboot/demo/springbootdemoapp/coach/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.springbootdemoapp.coach;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 min!!!!!";
+    }
+}
