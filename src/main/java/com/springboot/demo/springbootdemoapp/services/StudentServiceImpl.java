ed586d1bc78b29076f08e64d8f80b026d22ee05c
linda2927 <linda2927@naver.com>
2023-03-28 19:17:35 +0900

    Spring Boot Security: add 'in-memory' user details and requestMatchers

--- src/main/java/com/springboot/demo/springbootdemoapp/services/StudentServiceImpl.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/services/StudentServiceImpl.java
@@ -7,7 +7,6 @@ import com.springboot.demo.springbootdemoapp.repositories.StudentRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
