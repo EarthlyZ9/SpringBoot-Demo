3805ef315f3da66fc85aaa6165f36e2f103b3aba
linda2927 <linda2927@naver.com>
2023-03-28 19:37:49 +0900

    Spring Boot Security: use default spring security tables for jdbc authentication

--- src/main/java/com/springboot/demo/springbootdemoapp/config/DemoSecurityConfig.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/config/DemoSecurityConfig.java
@@ -1,39 +1,22 @@
 package com.springboot.demo.springbootdemoapp.config;
 
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC
+    // table: users, authorities
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-            .username("john")
-            .password("{noop}test123")
-            .roles("EMPLOYEE")
-            .build();
-
-        UserDetails mary = User.builder()
-            .username("mary")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER")
-            .build();
-
-        UserDetails susan = User.builder()
-            .username("susam")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER", "ADMIN")
-            .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     // Restricting access based on roles
@@ -56,5 +39,28 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder()
+//            .username("john")
+//            .password("{noop}test123")
+//            .roles("EMPLOYEE")
+//            .build();
+//
+//        UserDetails mary = User.builder()
+//            .username("mary")
+//            .password("{noop}test123")
+//            .roles("EMPLOYEE", "MANAGER")
+//            .build();
+//
+//        UserDetails susan = User.builder()
+//            .username("susam")
+//            .password("{noop}test123")
+//            .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//            .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 
 }
